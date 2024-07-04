0a849323fb84bdd32f8ff7940f1e37106e10da58
ariyan-321 <ariyan555777@gmail.com>
2024-07-04 07:43:12 +0600

    exicuted

---
+++ index.js
@@ -0,0 +1,19 @@
+let mark=prompt("Enter the mark");
+if (mark >=90 && mark<=100){
+  alert("You got AA")
+}else if (mark >=80 && mark<=89){
+  alert("You got A+")
+}else if (mark >=70 && mark<=79){
+  alert("You got A")
+}else if (mark >=60 && mark<=69){
+  alert("You got A-")
+}else if (mark >=50 && mark<=59){
+  alert("You got B")
+}else if (mark >=40 && mark<=49){
+  alert("You got D")
+}else if (mark > 0 && mark<33){
+  alert("You got F")
+}else{
+  alert("Invalid number")
+}
+
---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("line1");
+
+
+setTimeout(function(){
+  console.log("line2");
+},
+2000);
+
+console.log("line3");
+
